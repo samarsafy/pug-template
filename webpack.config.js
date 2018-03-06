1933ba8bdf066370561aff77b965aab2bfbcdaa6
Samar <samarsafy@yahoo.de>
2018-03-06 16:07:30 +0100

    changed the index.pug through the config, and added an objct inside array

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 var config = {
- entry: './src/main.js',
- output: {
-   path: path.resolve(__dirname, "dist"),
-   filename: 'bundle.js',
- },
- plugins: [
-   new HtmlWebpackPlugin({
-     template: '!!pug-loader!src/index.pug',
-   }),
- ]
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Pug Template',
+      h2: 'Checkout',
+      template: '!!pug-loader!src/index.pug',
+      posts: [{
+        name: 'Tomate',
+        price: 1.50 
+      }, 
+      {name: 'carrots',
+      price: 2}, {
+      name: 'apple', 
+      price: 4
+      }]
+    }),
+  ]
 }
 module.exports = config;
